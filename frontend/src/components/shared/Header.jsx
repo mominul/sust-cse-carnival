dd5de17397158fee045319feed4d940a03bece6f
atik65 <atikhasan801@gmail.com>
2024-02-17 14:23:43 +0600

    feat: navbar is going on

---
+++ frontend/src/components/shared/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "../../../public/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import { NavMenu } from "./Navmenu";
+
+const Header = () => {
+  return (
+    <div className="shadow ">
+      <div className="bg-white py-2 flex justify-between items-center container mx-auto px-5">
+        {/* left logo part */}
+        <div className="flex gap-2 items-center">
+          <Link href="/" className="flex items-center">
+            <Image
+              height={100}
+              width={200}
+              className="w-12 sm:w-16"
+              src={logo}
+              alt="logo"
+            />
+            {/* <h1 className="font-semibold opacity-55">Best Deal</h1> */}
+          </Link>
+        </div>
+
+        {/* middle links */}
+
+        <div>
+          <NavMenu />
+        </div>
+
+        {/* right search bar */}
+        <div className="flex justify-end">search bar</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
