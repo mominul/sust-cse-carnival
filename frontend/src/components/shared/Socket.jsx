137bb76b6db68004eeeb25479c059d19beaff65d
atik65 <atikhasan801@gmail.com>
2024-02-17 22:01:34 +0600

    socket working v2

--- frontend/src/components/shared/Socket.jsx
+++ frontend/src/components/shared/Socket.jsx
@@ -1,9 +1,117 @@
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+// import io from "socket.io-client";
+// import WebSocket from "ws";
+// import { Button } from "../ui/button";
+// import { Input } from "../ui/input";
+
+// // const socket = io("http://localhost:3001"); //
+// // const socket = io("ws://4508-27-147-232-86.ngrok-free.app/ws/search/"); //
+// // const socket = io("https://socket-io-chat.now.sh"); //
+
+// const Socket = () => {
+//   const [messages, setMessages] = useState([]);
+//   const [newMessage, setNewMessage] = useState("");
+//   const [socket, setSocket] = useState(null);
+
+//   // useEffect(() => {
+//   //   // Connect to the WebSocket server
+
+//   //   // console.log("entered socket useEffect");
+//   //   const socket = io("http://localhost:3001");
+
+//   //   // console.log("passed socket instance");
+
+//   //   // make a socket.emit to the server to send the initial message -- query
+
+//   //   socket.emit("message", "Initial Hello from front end!");
+
+//   //   socket.on("message", (data) => {
+//   //     console.log("getting query inside useEffect");
+
+//   //     console.log(data);
+//   //     setMessages([...messages, data]);
+//   //   });
+
+//   //   setSocket(socket);
+//   // }, []);
+
+//   // const sendMessage = () => {
+//   //   // const newMessage = "Hello from front end!";
+
+//   //   console.log("sending message");
+//   //   socket.emit("message", newMessage);
+
+//   //   // if emit is successful, then add the message to the state
+
+//   //   // setMessages([...messages, newMessage]);
+//   // };
+
+//   useEffect(() => {
+//     // Connect to the WebSocket server
+
+//     // console.log("entered socket useEffect");
+//     const socket = io("ws://4508-27-147-232-86.ngrok-free.app/ws/search/");
+
+//     // console.log("passed socket instance");
+
+//     // make a socket.emit to the server to send the initial message -- query
+
+//     socket.emit("query", "search query from frontend");
+
+//     socket.on("message", (data) => {
+//       console.log("getting query inside useEffect");
+
+//       console.log(data);
+//       setMessages([...messages, data]);
+//     });
+
+//     setSocket(socket);
+//   }, []);
+
+//   const sendMessage = () => {
+//     // const newMessage = "Hello from front end!";
+
+//     console.log("sending message");
+//     socket.emit("query", newMessage);
+
+//     // if emit is successful, then add the message to the state
+
+//     // setMessages([...messages, newMessage]);
+//   };
+
+//   console.log("messages", messages);
+
+//   return (
+//     <div className="my-5">
+//       <div className="flex gap-1">
+//         <Input
+//           value={newMessage}
+//           onChange={(e) => setNewMessage(e.target.value)}
+//         />
+
+//         <Button onClick={sendMessage}>Send</Button>
+//       </div>
+
+//       <div>
+//         {messages.map((message, index) => (
+//           <p key={index}>{message}</p>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Socket;
+
 "use client";
 
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import WebSocket from "ws";
+// import WebSocket from "ws";
 import { Button } from "../ui/button";
+import WebSocket from "websocket";
 import { Input } from "../ui/input";
 
 // const socket = io("http://localhost:3001"); //
@@ -13,75 +121,44 @@ import { Input } from "../ui/input";
 const Socket = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const [socket, setSocket] = useState(null);
+  // const [socket, setSocket] = useState(null);
 
-  // useEffect(() => {
-  //   // Connect to the WebSocket server
-
-  //   // console.log("entered socket useEffect");
-  //   const socket = io("http://localhost:3001");
-
-  //   // console.log("passed socket instance");
-
-  //   // make a socket.emit to the server to send the initial message -- query
-
-  //   socket.emit("message", "Initial Hello from front end!");
-
-  //   socket.on("message", (data) => {
-  //     console.log("getting query inside useEffect");
-
-  //     console.log(data);
-  //     setMessages([...messages, data]);
-  //   });
-
-  //   setSocket(socket);
-  // }, []);
-
-  // const sendMessage = () => {
-  //   // const newMessage = "Hello from front end!";
-
-  //   console.log("sending message");
-  //   socket.emit("message", newMessage);
-
-  //   // if emit is successful, then add the message to the state
-
-  //   // setMessages([...messages, newMessage]);
-  // };
+  const socket = new WebSocket.w3cwebsocket(
+    "ws://400f-27-147-232-86.ngrok-free.app/ws/search/"
+  );
 
   useEffect(() => {
-    // Connect to the WebSocket server
+    socket.onopen = () => {
+      console.log("WebSocket connected");
+      // You may send an initial message here if required
+      // socket.send(JSON.stringify({ query: "Initial message" }));
+    };
 
-    // console.log("entered socket useEffect");
-    const socket = io("ws://4508-27-147-232-86.ngrok-free.app/ws/search/");
+    socket.onmessage = (message) => {
+      console.log("Received message:", message.data);
 
-    // console.log("passed socket instance");
+      // setMessages([...messages, message.data]);
+      // Handle incoming messages from the server
+    };
 
-    // make a socket.emit to the server to send the initial message -- query
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+      // Handle WebSocket errors
+    };
 
-    socket.emit("query", "search query from frontend");
-
-    socket.on("message", (data) => {
-      console.log("getting query inside useEffect");
-
-      console.log(data);
-      setMessages([...messages, data]);
-    });
-
-    setSocket(socket);
+    socket.onclose = () => {
+      console.log("WebSocket closed");
+      // Handle WebSocket closure
+    };
   }, []);
 
   const sendMessage = () => {
-    // const newMessage = "Hello from front end!";
-
     console.log("sending message");
-    socket.emit("query", newMessage);
-
-    // if emit is successful, then add the message to the state
-
-    // setMessages([...messages, newMessage]);
+    socket.send(JSON.stringify({ query: "apple m1" }));
+    // socket.send(JSON.stringify({ query: newMessage }));
   };
 
-  console.log("messages", messages);
+  // console.log("messages", messages);
 
   return (
     <div className="my-5">
